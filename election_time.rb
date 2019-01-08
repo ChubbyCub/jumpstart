d66224f4687dcf28dd333a8dfcd10daedb2e1810
Elise Pham <elise.pham88@gmail.com>
2019-01-08 12:18:14 -0800

    Update election_time.rb

--- election_time.rb
+++ election_time.rb
@@ -1,5 +1,5 @@
-print "Welcome to my election voting program!\n"
-print "Election candidates are: Donald Duck, Minnie Mouse, Goofy\n"
+puts "Welcome to my election voting program!"
+puts "Election candidates are: Donald Duck, Minnie Mouse, and Goofy"
 
 #store user inputs
 temp_array = Array.new
@@ -9,55 +9,42 @@ temp_array = Array.new
   temp_array << vote
 end
 
-for i in 0...temp_array.length do
+temp_array.each_index {|i|
   puts "Vote ##{i + 1}: #{temp_array[i]}"
-end
+}
 
 #create hash to count votes
 vote_result = Hash.new
 
-for i in 0...temp_array.length do
-  if(!vote_result.has_key?(temp_array[i]))
-    vote_result.merge!(temp_array[i] => 0)
-  end
-end
-
-#puts "#{vote_result}"
-
-for i in 0...temp_array.length do
-  if(vote_result.has_key?(temp_array[i]))
-    vote_result[temp_array[i]] = vote_result[temp_array[i]] + 1
-  end
+for name in temp_array
+   if(vote_result.has_key?(name))
+      vote_result[name] = 1 + vote_result[name]
+    else
+      vote_result[name] = 1
+   end
 end
 
 #print vote results
-print "ELECTION RESULTS......\n"
-for i in 0...vote_result.length do
-  if (vote_result.values[i] <= 1)
-    puts "#{vote_result.keys[i]}: #{vote_result.values[i]} vote"
-  else
-    puts "#{vote_result.keys[i]}: #{vote_result.values[i]} votes"
-  end
+puts "ELECTION RESULTS......"
+vote_result. each do |key, value|
+  puts "#{key}: #{value} vote(s)"
 end
 
 #Find the winner and handle ties
 reverse_map = Hash.new
 
-for i in 0...vote_result.length do
-  if(!reverse_map.has_key?(vote_result.values[i]))
-    reverse_map[vote_result.values[i]] = [vote_result.keys[i]]
+vote_result.each {|key, value|
+  if(!reverse_map.has_key?(value))
+    reverse_map.merge!({value => [key]})
   else
-    reverse_map[vote_result.values[i]].push(vote_result.keys[i])
+    reverse_map[value] << key
   end
-end
+}
 
-#print "#{reverse_map}\n"
+puts "The winner is #{vote_result.key(vote_result.values.max)}"
 
-
-reverse_map.each { |k, v| puts "The winner is #{v}" if k == reverse_map.keys.max }
-
-for i in 0...reverse_map.length do
-  if(reverse_map.values[i].length > 1)
-    puts "The following candidates #{reverse_map.values[i]} are tied. Each got #{reverse_map.keys[i]} votes"
+reverse_map.each do |key, value|
+  if(value.length > 1)
+    puts "The following candidates #{reverse_map[key]} are tied. Each got #{key} votes"
   end
 end
