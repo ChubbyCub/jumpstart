77c2dd7e446d1b296bd294a729ab5657e173038b
Elise Pham <elise.pham88@gmail.com>
2019-01-07 19:10:32 -0800

    Update day2_exercises.rb

--- candy_machine.rb
+++ candy_machine.rb
@@ -13,16 +13,6 @@ puts "$#{money}, that's all?
 Well, lemme tell ya what we got here.\n"
 
 #print menu
-print "A. $0.65 Twix\nB. $0.50 Chips\nC. $0.75 Nutter Butter\nD. $0.65 Peanut Butter Cup\nE. $0.55 Juicy Fruit Gum\n"
-print "So, what'll ya have? >"
-answer = gets.chomp.upcase
-while (answer != "A" && answer != "B" && answer != "C" && answer != "D" && answer != "E")
-  puts "Your input is invalid. Try again"
-  puts "So, what'll ya have? >"
-  answer = gets.chomp.upcase
-end
-
-#calculate change and output response
 table = {
   "A" => 0.65,
   "B" => 0.50,
@@ -30,17 +20,21 @@ table = {
   "D" => 0.65,
   "E" => 0.55
 }
+print "A. $0.65 Twix\nB. $0.50 Chips\nC. $0.75 Nutter Butter\nD. $0.65 Peanut Butter Cup\nE. $0.55 Juicy Fruit Gum\n"
+print "So, what'll ya have? >"
+answer = gets.chomp.upcase
+while (!table.key?(answer))
+  puts "Your input is invalid. Try again"
+  puts "So, what'll ya have? >"
+  answer = gets.chomp.upcase
+end
 
-for i in 0..table.length do
-  if(answer == table.keys[i])
-    change = (money - table.values[i]).to_f
-      if(change < 0)
-        puts "You're broke. Take your $#{money} and go elsewhere."
-      elsif(change > 0)
-        puts "Please take your candy, and your $#{change} change!"
-      else
-        puts "Please take your candy!"
-      end
-      break
-  end
+#calculate change and output response
+change = (money - table[answer]).to_f
+if(change < 0)
+  puts "You're broke. Take your $#{money} and go elsewhere."
+elsif(change > 0)
+  puts "Please take your candy, and your $#{change} change!"
+else
+  puts "Please take your candy!"
 end
--- day2_exercises.rb
+++ day2_exercises.rb
@@ -1,36 +1,33 @@
-#problem 1
+puts "Question 1: Prompt for a number. If the number is greater than 70, print
+        PASSING; otherwise, print NOT PASSING"
+print "Please enter a number: "
 input_one = gets.chomp.to_i
-if(input_one > 70)
-  puts "PASSING"
-else
-  puts "NOT PASSING"
-end
+puts (input_one > 70 ? "PASSING" : "NOT PASSING")
 
-#problem 2
+puts "---------------------------------------------------------------------"
+puts "Question 2: Prompt for a string. If the string is equal to green, print GO,
+    otherwise, print STOP"
+print "Please enter a word: "
 input_two = gets.chomp
-if(input_two == "green")
-  puts "GO"
-else
-  puts "STOP"
-end
+puts (input_two == "green" ? "GO" : "STOP")
 
-#problem 3
+puts "---------------------------------------------------------------------"
+puts "Question 3: Prompt for a number. If the number is even, print EVEN, otherwise, print ODD"
+print "Please enter a number: "
 input_three = gets.chomp.to_i
-if(input_three % 2 == 0)
-  puts "EVEN"
-else
-  puts "ODD"
-end
+puts (input_three % 2 == 0 ? "EVEN" : "ODD")
 
-#problem 4
+puts "---------------------------------------------------------------------"
+puts "Question 4: Prompt for a number. If the number is evenly divisible by 5, print MULTIPLE
+    OF 5, otherwise, print NOT A MULTIPLE OF 5"
+print "Please enter a number: "
 input_four = gets.chomp.to_i
-if(input_four % 5 == 0)
-  puts "MULTIPLE OF 5"
-else
-  puts "NOT A MULTIPLE OF 5"
-end
+puts (input_four % 5 == 0 ? "MULTIPLE OF 5" : "NOT A MULTIPLE OF 5")
 
-#problem 5
+puts "---------------------------------------------------------------------"
+puts "Question 5: Prompt for a number. If the number is less than 10, print ONE DIGIT. If
+    the number is 100 or greater, print THREE DIGITS, otherwise print TWO DIGITS"
+print "Please enter a number: "
 input_five = gets.chomp.to_i
 if(input_five < 10)
   puts "ONE DIGIT"
@@ -40,27 +37,37 @@ else
   puts "TWO DIGITS"
 end
 
-#question 6
+puts "---------------------------------------------------------------------"
+puts "Question 6: Prompt for a jersey number. If that number is 12, 71, or 80, print That
+  number is retired from the Seattle Seahawks!, otherwise do nothing"
+print "Please enter a jersey number: "
 jersey_number = gets.chomp.to_i
 if(jersey_number == 12 || jersey_number == 71 || jersey_number == 80)
   puts "That number is retired from the Seattle Seahawks!"
 end
 
-#question 7
+puts "---------------------------------------------------------------------"
+puts "Question 7: Prompt for a state. If the state is Washington, Oregon, or Idaho, print
+  This state is in the PNW, otherwise print You should move to the PNW;
+  it’s great here!"
+print "Please enter a state name: "
 state = gets.chomp
-if(state == "Washington" || state == "Oregon" || state == "Idaho")
-  puts "This state is in the PNW"
-else
-  puts "You should move to the PNW; it’s great here!"
-end
+puts ((state == "Washington" || state == "Oregon" || state == "Idaho") ?
+"This state is in the PNW" : "You should move to the PNW; it’s great here!")
 
-#question 8
+puts "---------------------------------------------------------------------"
+puts "Question 8: Prompt for a one of the following: SHORT, TALL, GRANDE, VENTI.
+    Print out the number of ounces that drink includes (8, 12, 16, 20 respectively)."
 size = {"SHORT" => 8, "TALL" => 12, "GRANDE" => 16, "VENTI" => 20}
 print "Please enter your drink size: "
 input_size = gets.chomp
-puts "#{size[input_size]}"
+puts "#{size[input_size]} oz"
 
-#question 9
+puts "---------------------------------------------------------------------"
+puts "Question 9: Prompt for rate of pay and hours worked. Calculate gross pay.
+  Provide time-and-a-half for hours worked beyond 40 (e.g., if you
+    get paid $10/hr and work 45 hours in a week,
+    you would gross $475 (40 x 10 + 5 x 15)."
 print "What is your pay rate? > "
 pay_rate = gets.chomp.to_i
 print "How many hours did you work? > "
@@ -74,7 +81,10 @@ else
   puts "#{total_pay}"
 end
 
-#question 10
+puts "---------------------------------------------------------------------"
+puts "Question 10: Rewrite the solution to the previous problem adding this modification:
+  do not process any employee if their hours worked is greater than 60,
+    instead display the message Please see manager"
 print "What is your pay rate? > "
 pay_rate = gets.chomp.to_i
 print "How many hours did you work? > "
